d81f2d8dff943762bb3fabe96c2eeecafba136a3
wendyhamel <whamel@it4web.nl>
2022-09-28 15:36:58 +0200

    Setup tailwind, colors and font

---
+++ js/tailwind-config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['League Spartan', 'sans-serif'],
+        },
+        colors: {
+            'white': 'hsl(0, 0%, 100%)',
+            'gray-dark': 'hsl(0, 0%, 63%)',
+            'gray-very-dark': 'hsl(0, 0%, 27%)',
+            'black' : 'hsl(0, 0%, 0%)',
+
+        },
+        extend: {
+            fontSize: {
+                xxs: '0.625rem'
+            }
+        }
+    }
+}
