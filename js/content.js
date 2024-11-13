81e2692a3323fe6700b9ea7fc97c9357c816d9a0
wendyhamel <whamel@it4web.nl>
2024-11-13 13:19:13 +0100

    Added tailwind to project, worked on code improvements and working on transitions

--- js/content.js
+++ js/content.js
@@ -1,53 +1,72 @@
 window.content = function () {
     return {
+        menuItems: [
+            {
+                name: 'home',
+                href: '#',
+            },
+            {
+                name: 'shop',
+                href: '#',
+            },
+            {
+                name: 'about',
+                href: '#',
+            },
+            {
+                name: 'contact',
+                href: '#',
+            }
+        ],
         slides: [
             {
-                slide: 1,
                 image: {
                     mobile:'./images/mobile-image-hero-1.jpg',
                     desktop:'./images/desktop-image-hero-1.jpg',
                     altText: 'white chairs and a table'
                 },
                 title: 'Discover innovative ways to decorate',
-                description: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
+                description: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
+                href: '#'
             },
             {
-                slide: 2,
                 image: {
                     mobile:'./images/mobile-image-hero-2.jpg',
                     desktop:'./images/desktop-image-hero-2.jpg',
                     altText: 'chairs of different colors'
                 },
                 title: 'We are available all across the globe',
-                description: 'With stores all over the world, it\'s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don\'t hesitate to contact us today.'
+                description: 'With stores all over the world, it\'s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don\'t hesitate to contact us today.',
+                href: '#'
             },
             {
-                slide: 3,
                 image: {
                     mobile:'./images/mobile-image-hero-3.jpg',
                     desktop:'./images/desktop-image-hero-3.jpg',
                     altText: 'black chair'
                 },
                 title: 'Manufactured with the best materials',
-                description: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
+                description: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
+                href: '#'
             }
         ],
-        setSlide: 1,
+        showSlide: 0,
         animation: false,
-        get slide() {
-            slidesArray = this.slides.filter(slide => slide.slide === this.setSlide);
-            return slidesArray[0];
+        previousSlide(index) {
+            if(index === 0) {
+                this.showSlide = this.slides.length-1
+            } else {
+                this.showSlide--
+            }
         },
-        previousSlide() {
-            return this.setSlide === 1 ? this.setSlide = 3 : this.setSlide === 2 ? this.setSlide = 1 : this.setSlide === 3 ? this.setSlide = 2 : this.setSlide = 1
+        nextSlide(index) {
+            if(index === this.slides.length-1) {
+                this.showSlide = 0
+            } else {
+                this.showSlide++
+            }
+
         },
-        nextSlide() {
-            return this.setSlide === 1 ? this.setSlide = 2 : this.setSlide === 2 ? this.setSlide = 3 : this.setSlide === 3 ? this.setSlide = 1 : this.setSlide = 1
-        },
-        animate() {
-            this.animation = true;
-            setTimeout(function() {this.animation = false}.bind(this) ,800);
-        }
     }
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/content.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['League Spartan', 'sans-serif'],
+      },
+      colors: {
+        'white': 'hsl(0, 0%, 100%)',
+        'gray-dark': 'hsl(0, 0%, 63%)',
+        'gray-very-dark': 'hsl(0, 0%, 27%)',
+        'black' : 'hsl(0, 0%, 0%)',
+      },
+      fontSize: {
+        xxs: '0.625rem'
+      },
+      maxWidth: {
+        '1440': '1440px'
+      },
+      scale: {
+        '102.5': '1.025'
+      },
+      transitionDuration: {
+        600: '600ms'
+      }
+    },
+  },
+}
+
